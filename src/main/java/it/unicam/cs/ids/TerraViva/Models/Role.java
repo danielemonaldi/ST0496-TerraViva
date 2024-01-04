bf29dc0ad68baff617f8810beeaf37c843a4cb43
Daniele Monaldi <daniele-monaldi@hotmail.it>
2024-01-04 11:26:51 +0100

    Added User class, User repository and User roles.

---
+++ src/main/java/it/unicam/cs/ids/TerraViva/Models/Role.java
@@ -0,0 +1,11 @@
+package it.unicam.cs.ids.TerraViva.Models;
+
+public enum Role {
+    AUTHORIZED_TOURIST,
+    CONTRIBUTOR,
+    AUTHORIZED_CONTRIBUTOR,
+    ENTERTAINER,
+    AUTHORIZED_ENTERTAINER,
+    MANAGER,
+    CURATOR
+}
---
+++ src/main/java/it/unicam/cs/ids/TerraViva/Models/User.java
@@ -0,0 +1,66 @@
+package it.unicam.cs.ids.TerraViva.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Entity
+@Table(name = "USERS")
+public class User implements UserDetails {
+
+    @Id
+    private String username;
+    private String password;
+    private String email;
+
+    public User() {}
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
