26cee9bf8e44a12f7047d11d3c5ac74a633cca81
Frangiosc <fmorici2002@gmail.com>
2024-01-14 11:01:43 +0100

    Implemented multimedia content creation.

--- src/main/java/it/unicam/cs/ids/TerraViva/Controllers/ContentController.java
+++ src/main/java/it/unicam/cs/ids/TerraViva/Controllers/ContentController.java
@@ -3,6 +3,7 @@ package it.unicam.cs.ids.TerraViva.Controllers;
 import it.unicam.cs.ids.TerraViva.Models.ToAuthorize.Contributes.MultimediaContent;
 import it.unicam.cs.ids.TerraViva.Models.ToAuthorize.Contributes.TextualContent;
 import it.unicam.cs.ids.TerraViva.Models.ToAuthorize.POI;
+import it.unicam.cs.ids.TerraViva.Services.MultimediaContentServices;
 import it.unicam.cs.ids.TerraViva.Services.TextualContentServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class ContentController {
     @Autowired
     TextualContentServices textualContentServices;
+
+    @Autowired
+    MultimediaContentServices multimediaContentServices;
 
     @PostMapping("/creation/content/textual")
     public ResponseEntity<String> create(@RequestBody TextualContent content){
@@ -29,7 +33,7 @@ public class ContentController {
     @PostMapping("/creation/content/multimedia")
     public ResponseEntity<String> create(@RequestBody MultimediaContent content){
         try {
-            //poiServices.publish(content);
+            multimediaContentServices.publish(content);
             return ResponseEntity.status(HttpStatus.CREATED).body("Content uploaded successfully");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error uploading content: " + e.getMessage());
