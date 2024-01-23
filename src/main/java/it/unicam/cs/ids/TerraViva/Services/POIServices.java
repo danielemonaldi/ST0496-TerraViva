5c153e09fd43008c2a5a003c2b906e1099a0155a
Andrea Malloni <malloni.andrea02@gmail.com>
2024-01-23 18:17:35 +0100

    Changed POIServices to match UML Model.

--- src/main/java/it/unicam/cs/ids/TerraViva/Services/POIServices.java
+++ src/main/java/it/unicam/cs/ids/TerraViva/Services/POIServices.java
@@ -1,13 +1,15 @@
 package it.unicam.cs.ids.TerraViva.Services;
 
-import it.unicam.cs.ids.TerraViva.Models.ToAuthorize.POI;
+import it.unicam.cs.ids.TerraViva.Models.ToAuthorize.POI.*;
 import it.unicam.cs.ids.TerraViva.Models.Requests.AuthorizationRequest;
+import it.unicam.cs.ids.TerraViva.Models.User;
 import it.unicam.cs.ids.TerraViva.Repository.AuthorizationRepository;
 import it.unicam.cs.ids.TerraViva.Repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class POIServices {
@@ -20,7 +22,31 @@ public class POIServices {
     @Autowired
     private AuthRequestServices requestServices;
 
-    public void publish(POI poi) throws Exception {
+    public CulturalPOI createCulturalPOI(double latitude, double longitude, User author) {
+        return new CulturalPOI(latitude, longitude, author);
+    }
+
+    public EventPOI createEventPOI(double latitude, double longitude, User author) {
+        return new EventPOI(latitude, longitude, author);
+    }
+
+    public RecreationalPOI createRecreationalPOI(double latitude, double longitude, User author) {
+        return new RecreationalPOI(latitude, longitude, author);
+    }
+
+    public CommercialPOI createCommercialPOI(double latitude, double longitude, User author) {
+        return new CommercialPOI(latitude, longitude, author);
+    }
+
+    public void delete(POI poi) {
+        poiRepository.delete(poi);
+    }
+
+    public List<POI> getPOI() {
+        return poiRepository.findAll();
+    }
+
+    public void confirmNew(POI poi) {
         Date creation = new Date(System.currentTimeMillis());
         AuthorizationRequest request = new AuthorizationRequest(poi.getAuthor(), poi, creation);
         poiRepository.save(poi);
